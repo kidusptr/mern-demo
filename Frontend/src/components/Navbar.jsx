25ca76f0151d536756718910f028dd0b67139981
Kidus Petros <kidusptr@gmail.com>
2024-09-15 15:31:26 +0300

    Prepared a clean entry point for the frontend by separating pages and components

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text></Text>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
