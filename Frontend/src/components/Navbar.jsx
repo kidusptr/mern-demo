6eb5a8f67be746587238c66e8d736053562a5666
Kidus Petros <kidusptr@gmail.com>
2024-09-15 16:01:21 +0300

    Implemented the navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
-import { Container, Flex, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Text,
+  Link,
+  HStack,
+  Button,
+  useColorMode,
+} from "@chakra-ui/react";
+import { PlusSquareIcon } from "@chakra-ui/icons";
 import React from "react";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -10,7 +20,27 @@ const Navbar = () => {
         justifyContent={"space-between"}
         flexDir={{ base: "column", sm: "row" }}
       >
-        <Text></Text>
+        <Text
+          bgGradient="linear(to-r, cyan.400, blue.500)"
+          bgClip="text"
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight="bold"
+        >
+          <Link to={"/"}>My Store 🛒</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/add"}>
+            <Button>
+              <PlusSquareIcon />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? "Dark" : "Light"}
+          </Button>
+        </HStack>
       </Flex>
     </Container>
   );
