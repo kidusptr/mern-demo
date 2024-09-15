569765d9d19848cb271049e5b1c38199cc1430fe
Kidus Petros <kidusptr@gmail.com>
2024-09-15 20:19:47 +0300

    Preparing for deploymnet

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
 import productRoute from "./routes/product.route.js";
 
@@ -7,9 +9,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 1000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use("/api/products", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/Frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("Hola migo!");
